15365276396853629eee379c2a1d7b5d99a18304
VishwasIyengar <vishwassv1995@gmail.com>
2024-08-05 19:47:11 +0530

    modified codes

--- src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
+++ src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -25,27 +24,21 @@ public class BrowserWindowsTest {
 
     @BeforeClass
     public void setUp() {
+        // Set the path to chromedriver executable
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");  // Set a fixed window size for consistency in headless mode
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/browser-windows");
+        driver.manage().window().setSize(new Dimension(1920, 1080));
         mainWindowHandle = driver.getWindowHandle();
     }
 
-
     @Test
     public void testNewTab() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));  // Increase wait time
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Ensure the "New Tab" button is scrolled into view
-        WebElement newTabButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("tabButton")));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", newTabButton);
-        wait.until(ExpectedConditions.elementToBeClickable(newTabButton));
-        
+        // Click on the "New Tab" button when it is clickable
+        WebElement newTabButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("tabButton")));
         try {
             newTabButton.click();
         } catch (ElementClickInterceptedException e) {
