ee7d775936abef153b614bf59c9f30c09d4d785e
VishwasIyengar <vishwassv1995@gmail.com>
2024-08-05 16:24:01 +0530

    modified script

--- src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/AmazonIPhonePurchaseTest.java
+++ src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/AmazonIPhonePurchaseTest.java
@@ -6,69 +6,75 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class AmazonIPhonePurchaseTest 
 {
-     WebDriver driver;
-     WebDriverWait wait;
-    
-    @SuppressWarnings("deprecation")
-	@BeforeClass
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
     public void setUp() 
     {
         // Set up WebDriver
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    	driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
         
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
-        
 
     @Test
     public void testIPhonePurchase() throws InterruptedException 
     {
-    	// Search for iPhone
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]"));
+        // Search for iPhone
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
         searchBox.sendKeys("iPhone");
         
-        WebElement searchButton = driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]"));
+        WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
         searchButton.click();
         
         WebElement secondExpensiveIPhone = driver.findElement(By.linkText("Apple iPhone 15 Plus (128 GB) - Black"));
         secondExpensiveIPhone.click();
         
         Set<String> s = driver.getWindowHandles();
-        ArrayList ar = new ArrayList(s);
-        System.out.println(ar.get(0));
-        System.out.println(ar.get(1));
-        driver.switchTo().window((String)ar.get(1));
+        ArrayList<String> ar = new ArrayList<>(s);
+        driver.switchTo().window(ar.get(1));
         
-        WebElement addCard = driver.findElement(By.xpath("//div[@class='a-section a-spacing-none a-padding-none']//input[@id='add-to-cart-button']"));
+        WebElement addCard = driver.findElement(By.xpath("(//input[@id='add-to-cart-button'])[2]"));
         addCard.click();
         
         WebElement proceedToCheckOut = driver.findElement(By.xpath("(//input[@aria-labelledby='attach-sidesheet-checkout-button-announce'])[1]"));
         proceedToCheckOut.click();
         
+        Thread.sleep(2000);
         WebElement enterEmail = driver.findElement(By.xpath("//input[@id='ap_email_login']"));
         enterEmail.sendKeys("vishwassv1995@gmail.com");
+        
+        Thread.sleep(2000);
         
         WebElement continueButton = driver.findElement(By.xpath("//input[@type='submit']"));
         continueButton.click();
         
+        Thread.sleep(2000);
+        
         WebElement enterPassword = driver.findElement(By.xpath("//input[@id='ap_password']"));
         enterPassword.sendKeys("Vishu@2024");
+        
+        Thread.sleep(2000);
         
         WebElement signInButton = driver.findElement(By.xpath("//input[@id='signInSubmit']"));
         signInButton.click();
@@ -107,13 +113,12 @@ public class AmazonIPhonePurchaseTest
          } 
         Thread.sleep(5000);
     }
-      
     
+      
     @AfterClass
     public void tearDown() 
     {       
         driver.quit();
-	}
-        
-	 
+    }	 
 }
+
--- src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
+++ src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
@@ -1,61 +1,72 @@
 package AutomationRestAPI.SeleniumAutomationRestAPI;
 
-import java.util.Set; 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class BrowserWindowsTest 
-{
-     WebDriver driver;
-     String mainWindowHandle;
+import java.time.Duration;
+import java.util.Set;
+
+public class BrowserWindowsTest {
+
+    WebDriver driver;
+    String mainWindowHandle;
 
     @BeforeClass
-    public void setUp() 
-    {
-        // Set the path to chromedriver executable
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver.exe");
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://demoqa.com/browser-windows");
         mainWindowHandle = driver.getWindowHandle();
-        driver.manage().window().maximize();
     }
 
     @Test
-    public void testNewTab() throws InterruptedException 
-    {
+    public void testNewTab() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         // Click on the "New Tab" button
         WebElement newTabButton = driver.findElement(By.id("tabButton"));
         newTabButton.click();
 
         // Switch to the new tab
         Set<String> allWindowHandles = driver.getWindowHandles();
-        for (String handle : allWindowHandles) 
-        {
-            if (!handle.equals(mainWindowHandle)) 
-            {
+        for (String handle : allWindowHandles) {
+            if (!handle.equals(mainWindowHandle)) {
                 driver.switchTo().window(handle);
                 break;
             }
         }
 
-        // Assert the title of the new tab
-        String expectedTitle = "This is a sample page";
-        String actualTitle = driver.getTitle();
-        Assert.assertNotEquals(actualTitle, expectedTitle);
+        // Wait for the new tab to load content
+        WebElement heading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sampleHeading")));
+
+        // Assert the heading of the new tab
+        String expectedHeading = "This is a sample page";
+        String actualHeading = heading.getText();
+        Assert.assertEquals(actualHeading, expectedHeading);
+
         Thread.sleep(5000);
     }
-    
-	@AfterClass
-    public void tearDown() 
-	{
-        // Close the new tab
-        driver.close();
+
+    @AfterClass
+    public void tearDown() {
+        // Close all tabs except the main window
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String handle : allWindowHandles) {
+            if (!handle.equals(mainWindowHandle)) {
+                driver.switchTo().window(handle);
+                driver.close();
+            }
+        }
 
         // Switch back to the main window
         driver.switchTo().window(mainWindowHandle);
@@ -64,6 +75,7 @@ public class BrowserWindowsTest
         driver.quit();
     }
 }
+
 
 
 
