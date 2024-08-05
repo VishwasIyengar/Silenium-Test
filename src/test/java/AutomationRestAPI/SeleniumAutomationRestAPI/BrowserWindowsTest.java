c01915585c75518ece4a1baf93a1f4e923e8aa6b
VishwasIyengar <vishwassv1995@gmail.com>
2024-08-05 12:50:56 +0530

    automation

--- src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
+++ src/test/java/AutomationRestAPI/SeleniumAutomationRestAPI/BrowserWindowsTest.java
@@ -1,6 +1,6 @@
 package AutomationRestAPI.SeleniumAutomationRestAPI;
 
-import java.util.Set; 
+import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
